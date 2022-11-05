27ff66783f16ac0e1f54160ddafcaf7ae9bca73d
Benjamin Ejike <benmma13@gmail.com>
2022-11-05 17:39:33 +0400

    Inser data into animals table and queried the data

--- data.sql
+++ data.sql
@@ -7,3 +7,14 @@ INSERT INTO animals (name,
 ('Pikachu', '2021-01-07', 0, False, 15.04),
 ('Devimon', '2017-05-12', 5, True, 11);
 
+-- Tuesday
+
+INSERT INTO animals (name,
+date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES ('Charmander', '2020-02-08', 0, False, -11),
+('Plantmon', '2021-11-15', 2, True, -5.7),
+('Squirtle', '1993-04-02', 3, False, -12.13),
+('Angemon', '2005-06-12', 3, False, -45),
+('Boarmon', '2005-06-07', 7, True, 20.4),
+('Blossom', '1998-10-13', 3, True, 17),
+('Ditto', '2022-05-14', 4, True, 22);
--- queries.sql
+++ queries.sql
@@ -10,3 +10,48 @@ SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
 SELECT name, neutered FROM animals WHERE neutered = True;
 SELECT name FROM animals WHERE name != 'Gabumon';
 SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
+
+
+BEGIN;
+UPDATE animals
+SET species = 'digimon'
+WHERE animals.name LIKE '%mon';
+UPDATE animals
+SET species = 'pokemon'
+WHERE animals.name NOT LIKE '%mon';
+COMMIT;
+
+BEGIN;
+DELETE FROM animals;
+ROLLBACK;
+TABLE animals;
+
+BEGIN;
+DELETE FROM animals
+WHERE date_of_birth > '2022-01-01;
+SAVEPOINT SP1;
+UPDATE animals
+SET weight_kg = weight_kg * -1;
+ROLLBACK SP1;
+UPDATE animals
+SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
+COMMIT;
+
+-- aggregate
+
+SELECT COUNT(name) FROM animals;
+
+SELECT COUNT(escape_attempts) FROM animals 
+WHERE animals.escape_attempts = 0;
+
+SELECT AVG (weight_kg) FROM animals;
+
+SELECT neutered, SUM(escape_attempts) AS "Total escape attempts" 
+FROM animals GROUP BY neutered;
+
+SELECT species, MAX(weight_kg) AS "Maximum Weight", MIN(weight_kg) 
+AS "Minimum Weight" FROM animals GROUP BY species;
+
+SELECT species, AVG(escape_attempts) AS "Avg Escape Attempts" 
+FROM animals WHERE date_of_birth > '1990-01-01' 
+AND date_of_birth < '2000-12-31' GROUP BY species;
