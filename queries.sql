de7bee726928e3df64dbe920b19ded96f45c8f44
Benjamin Ejike <benmma13@gmail.com>
2023-01-25 23:12:07 +0400

    querries and aggregates

--- queries.sql
+++ queries.sql
@@ -28,13 +28,13 @@ TABLE animals;
 
 BEGIN;
 DELETE FROM animals
-WHERE date_of_birth > '2022-01-01;
+WHERE date_of_birth > '2022-01-01';
 SAVEPOINT SP1;
 UPDATE animals
 SET weight_kg = weight_kg * -1;
 ROLLBACK SP1;
 UPDATE animals
-SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
+SET weight_kg = weight_kg * -1
 COMMIT;
 
 -- aggregate
